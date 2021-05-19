cbf48e019f357a89ed99515d5a2493d38d4a1e04
crisanbianca <61552409+crisanbianca@users.noreply.github.com>
2021-05-19 19:32:11 +0300

    final version

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const app = express();
+app.use(requireHTTPS);
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(`./dist/<name-on-package.json>`));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
+);
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
